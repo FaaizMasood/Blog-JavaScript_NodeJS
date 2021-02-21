901eff0c3e72ae2986790c37e4fc80e6a4e8a2cd
Faaiz <fmasood@clarku.edu>
2021-02-20 22:43:06 -0600

    API endpoints unfinished

---
+++ Blog-API/exercise/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const Post = require("./api/models/posts");
+const { response } = require("express");
+const postData = new Post();
+
+app.get("/api/posts", (req, res) => {
+  //   const test = {
+  //     testing: "testing",
+  //   };
+  //   postData.add(test);
+  res.status(200).send(postData.get());
+});
+
+app.get("/api/posts/:post_id", (req, res) => {
+  const postId = req.params.post_id;
+  const foundPost = postData.getIndividualBlog(postId);
+  if (foundPost) {
+    res.status(200).send(foundPost);
+  } else {
+    res.status(404).send("Not Found");
+  }
+});
+
+app.listen(3000, () => console.log("listinening on  http://localhost:3000"));
